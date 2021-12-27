8c5b72bfd557ba7b30a2b6ea680efa568b4024c7
Tony D <adunc3@gmail.com>
2021-12-27 09:56:37 -0600

    Routing and wildcards work before i18n added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { Child1Component } from "./components/child1/child1.component";
+import { Child2Component } from "./components/child2/child2.component";
+import { HomeComponent } from "./components/home/home.component";
+
+const routes: Routes = [
+  { path: 'poc-home/child-1', component: Child1Component },
+  { path: 'poc-home/child-2', component: Child2Component },
+  { path: 'poc-home', component: HomeComponent },
+  { path: '**', redirectTo: 'poc-home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { Child1Component } from './components/child1/child1.component';
+import { Child2Component } from './components/child2/child2.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Child1Component,
+    Child2Component,
+    HomeComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
